623d2fe0106fee1deb10b82e6e5603bd7214c925
dansusman <susman.d@northeastern.edu>
2021-05-16 20:26:22 -0400

    Set up Git

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,16 @@
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "redditclone",
+  type: "postgresql",
+  user: "danielsusman",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
