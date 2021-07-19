a9fd14595e3687ced7987917fdfca8e75d0db39f
Daniel Susman <frenchminister@gmail.com>
2021-07-18 20:03:49 -0400

    Add Tab Info/Favicon

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,23 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>wroteit - A Simplified, Bootleg Reddit.</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-32x32.png"
+        />
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
